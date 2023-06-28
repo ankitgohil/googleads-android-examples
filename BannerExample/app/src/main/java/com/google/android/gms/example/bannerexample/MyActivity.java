9b5a40e407e80d23ddc303cc796f09f8f891458a
ankitgohil <ankitgohil5252@gmail.com>
2023-06-28 16:53:17 +0530

    latest

---
+++ BannerExample/app/src/main/java/com/google/android/gms/example/bannerexample/MyActivity.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2013 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.gms.example.bannerexample;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Main Activity. Inflates main activity xml and child fragments.
+ */
+public class MyActivity extends AppCompatActivity {
+
+  private static final String TAG = "MyActivity";
+  private final AtomicBoolean isMobileAdsInitializeCalled = new AtomicBoolean(false);
+  private AdView adView;
+  private GoogleMobileAdsConsentManager googleMobileAdsConsentManager;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_my);
+
+    // Log the Mobile Ads SDK version.
+    Log.d(TAG, "Google Mobile Ads SDK Version: " + MobileAds.getVersion());
+
+    // Set your test devices. Check your logcat output for the hashed device ID to
+    // get test ads on a physical device. e.g.
+    // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+    // to get test ads on this device."
+    MobileAds.setRequestConfiguration(
+        new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+            .build());
+
+    // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+    // values/strings.xml.
+    adView = findViewById(R.id.ad_view);
+
+    googleMobileAdsConsentManager = new GoogleMobileAdsConsentManager(this);
+
+    googleMobileAdsConsentManager.gatherConsent(
+        consentError -> {
+          if (consentError != null) {
+            // Consent not obtained in current session. This sample loads ads using
+            // consent obtained in the previous session.
+            Log.w(
+                TAG,
+                String.format(
+                    "%s: %s",
+                    consentError.getErrorCode(),
+                    consentError.getMessage()));
+          }
+
+          if (googleMobileAdsConsentManager.canRequestAds()) {
+            initializeMobileAdsSdk();
+          }
+
+          if (googleMobileAdsConsentManager.isFormAvailable()) {
+            // Regenerate the options menu to include a privacy setting.
+            invalidateOptionsMenu();
+          }
+        }
+    );
+
+    if (googleMobileAdsConsentManager.canRequestAds()) {
+      initializeMobileAdsSdk();
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.action_menu, menu);
+    MenuItem moreMenu = menu.findItem(R.id.action_more);
+    moreMenu.setVisible(googleMobileAdsConsentManager.isFormAvailable());
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    View menuItemView = findViewById(item.getItemId());
+    PopupMenu popup = new PopupMenu(this, menuItemView);
+    popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+    popup.show();
+    popup.setOnMenuItemClickListener(
+        popupMenuItem -> {
+          if (popupMenuItem.getItemId() == R.id.privacy_settings) {
+            // Handle changes to user consent.
+            googleMobileAdsConsentManager.presentPrivacyOptionsFormIfRequired(
+                this,
+                formError -> {
+                  if (formError != null) {
+                    Toast.makeText(
+                            this,
+                            formError.getMessage(),
+                            Toast.LENGTH_SHORT)
+                        .show();
+                  }
+                }
+            );
+            return true;
+          }
+          return false;
+        });
+    return super.onOptionsItemSelected(item);
+  }
+
+  /** Called when leaving the activity */
+  @Override
+  public void onPause() {
+    if (adView != null) {
+      adView.pause();
+    }
+    super.onPause();
+  }
+
+  /** Called when returning to the activity */
+  @Override
+  public void onResume() {
+    super.onResume();
+    if (adView != null) {
+      adView.resume();
+    }
+  }
+
+  /** Called before the activity is destroyed */
+  @Override
+  public void onDestroy() {
+    if (adView != null) {
+      adView.destroy();
+    }
+    super.onDestroy();
+  }
+
+  private void initializeMobileAdsSdk() {
+    if (isMobileAdsInitializeCalled.getAndSet(true)) {
+      return;
+    }
+
+    // Initialize the Google Mobile Ads SDK.
+    MobileAds.initialize(this);
+
+    // Load an ad.
+    AdRequest adRequest = new AdRequest.Builder().build();
+    adView.loadAd(adRequest);
+  }
+}
